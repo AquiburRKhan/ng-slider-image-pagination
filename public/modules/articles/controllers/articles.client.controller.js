b5956e8dec0fa406b44a6e7ed04b95fb4c10ac56
mahfuz <mahfuz.cracker@gmail.com>
2015-07-07 13:05:51 +0600

    Angular slider with image pagination

--- public/modules/articles/controllers/articles.client.controller.js
+++ public/modules/articles/controllers/articles.client.controller.js
@@ -64,5 +64,29 @@ angular.module('articles').controller('ArticlesController', ['$scope', '$statePa
 				articleId: $stateParams.articleId
 			});
 		};
+
+		$scope.currentPage = 0;
+		$scope.pageSize = 5;
+		$scope.images = ["http://everythingbirdsonline.com/wp-content/uploads/2014/06/birds-of-paradise.jpg",
+						"http://randomstory.org/wp-content/uploads/2013/07/44.jpg",
+						"http://www.photographyblogger.net/wp-content/uploads/2012/06/Brightly-Colored-Birds22.jpg",
+						"http://i.telegraph.co.uk/multimedia/archive/02649/birds-mouse_2649566k.jpg",
+						"http://creationrevolution.com/wp-content/uploads/2012/12/bird1.jpg",
+						"http://amolife.com/image/images/stories/beautiful%20birds/beautiful-birds-1.jpg",
+						"https://upload.wikimedia.org/wikipedia/commons/3/32/House_sparrow04.jpg",
+						"http://www.pageresource.com/wallpapers/wallpaper/birds-falcon-bird.jpg",
+						"http://news.bbcimg.co.uk/media/images/73826000/jpg/_73826131_trevorhannant-greattit.jpg",
+						"http://www.pageresource.com/wallpapers/wallpaper/bird-birds_569458.jpg"];
+
+		$scope.selectedPhoto=$scope.images[0];
+		$scope.numberOfPages=function(){
+			return Math.ceil($scope.images.length/$scope.pageSize);
+		}
+		$scope.select=function(url){
+			var index= $scope.images.indexOf(url);
+			$scope.selectedPhoto=$scope.images[index];
+		}
+
+
 	}
 ]);
